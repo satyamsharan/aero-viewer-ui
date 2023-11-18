19f6bd855054c529b4916a2caf23ecaee62ea7c4
Satyam Sharan <satyam.sharan@afry.com>
2023-11-18 13:19:42 +0100

    Added Next.js framework

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './ui/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Aero Viewer',
+  description: 'Realtime monitoring of ait quality in bypass (förbifart) Stockholm',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="h-screen w-screen flex justify-center items-center background-image">
+        <div >Aero Viewer</div>
+      </div>
+    </main>
+  )
+}
