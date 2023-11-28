775d9629398fe23044a74d7eb541f804439902f2
Satyam Sharan <satyam.sharan@afry.com>
2023-11-29 00:36:38 +0100

    Updated chart update

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -12,16 +12,54 @@ export default function Home() {
   const [brFra, setBrFra] = useState<number>(200);
   const [qdFra, setQdFra] = useState<number>(150);
   const [weather, setWeather] = useState<number[]>([10,-15,2]);
+  const [chartData1, setChartData1] = useState<number[][]>([]);
+  const [chartData2, setChartData2] = useState<number[][]>([]);
+  const [chartData3, setChartData3] = useState<number[][]>([]);
 
   useEffect(()=>{
     console.log('Ran first time')
+    const now = new Date();
+    const yesterday = new Date(now);
+    yesterday.setDate(yesterday.getDate()-1);
+    const offsetDate = new Date(yesterday);
+    offsetDate.setHours(0)
+    offsetDate.setMinutes(0)
+    offsetDate.setSeconds(0)
+    offsetDate.setMilliseconds(0)
+
+    const data1:number[][] = []
+    const data2:number[][] = []
+    const data3:number[][] = []
+    for (var d = new Date(yesterday); d <= now; d.setMinutes(d.getMinutes() + 5)) {
+        let time = (d.getTime() - offsetDate.getTime())/3000000;
+        data1.push([d.getTime(), 100 * Math.sin(Math.PI*time)])
+        data2.push([d.getTime(), 100 * Math.sin(Math.PI/4 + Math.PI*time)])
+        data3.push([d.getTime(), 100 * Math.sin(Math.PI/2 + Math.PI*time)])
+    }
+    setChartData1(data1)
+    setChartData2(data2)
+    setChartData3(data3)
+
     setInterval(()=>{
       console.log('Repeating every time')
       setInFra(a=>(a+10)%1000)
       setBrFra(a=>(a+15)%1000)
       setQdFra(a=>(a+20)%1000)
       setWeather(a=>[(a[0] + Math.round(Math.random()*100)/40)%100, (a[1] + Math.round(Math.random()*100)/40)%50, Math.round(Math.random()*1000)/100])
-    },5000);
+
+      var d = new Date();
+      let time = (d.getTime() - offsetDate.getTime())/3000000;
+      data1.pop();
+      data2.pop();
+      data3.pop();
+      data1.push([d.getTime(), 100 * Math.sin(Math.PI*time)])
+      data2.push([d.getTime(), 100 * Math.sin(Math.PI/4 + Math.PI*time)])
+      data3.push([d.getTime(), 100 * Math.sin(Math.PI/2 + Math.PI*time)])
+      setChartData1(data1)
+      setChartData2(data2)
+      setChartData3(data3)
+
+    },300000);
   },[]);
 
   return (
@@ -50,7 +88,7 @@ export default function Home() {
       </div>
         <Card>
           <CardBody>
-            <LineChart />
+            <LineChart data1={chartData1} data2={chartData2} data3={chartData3}/>
           </CardBody>
         </Card>
     </main>
